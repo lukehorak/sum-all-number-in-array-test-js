a4da85d647faac10f7fc3429479883fa764452ec
Lukas Horak <lukasbhorak@gmail.com>
2019-06-26 12:50:33 -0400

    did the thing!

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 function sumItems(array) {
   // Sum all the numbers in the array
+  let initial = 0;
+  array.forEach(thing => {
+    if (Array.isArray(thing)) {
+      initial += sumItems(thing);
+    }
+    else{
+      initial += thing;
+    }
+  })
+  return initial;
 }
 
 module.exports = sumItems;
